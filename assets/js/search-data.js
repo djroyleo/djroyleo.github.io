e6be4be1c336f9e93af589e5f5b6e4e318c2ec10
djroyleo <djroyleo@gmail.com>
2026-04-28 03:35:26 +0000

    Deploying to gh-pages from @ djroyleo/djroyleo.github.io@4f3d796d189a332784ab1cae2efd111991819a7c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-subset-selection",
+        
+          title: "Subset Selection",
+        
+        description: "My notes on subset selection methods from the textbook &quot;An Introduction to Statistical Learning&quot;",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/biostats-week4/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -358,17 +369,6 @@ ninja.data = [{
             window.location.href = "/blog/2021/diagrams/";
           
         },
-      },{id: "post-subset-selection",
-        
-          title: "Subset Selection",
-        
-        description: "My notes on subset selection methods from the textbook &quot;An Introduction to Statistical Learning&quot;",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/biostats-week4/";
-          
-        },
       },{id: "post-a-distill-style-blog-post",
         
           title: "a distill-style blog post",
@@ -572,21 +572,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/DylanRoyLeo_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%64%6A%72%6F%79%6C%65%6F@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -594,13 +587,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
